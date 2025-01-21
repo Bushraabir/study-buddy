89eb93cb437e17006a6fe368545bce4dbcff66a6
bushraabir <muzahidulislamabir123@gmail.com>
2025-01-21 18:51:21 +0600

    Remove all files and reset history

--- src/pages/3D.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const model = () => {
-  return (
-    <div>3D</div>
-  )
-}
-
-export default model
--- src/pages/StartSession.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StartSession = () => {
-  return (
-    <div>StartSession</div>
-  )
-}
-
-export default StartSession
