784a4b5a50f0c69e1c5ec80f9145fb47bc32aa2f
Bushraabir <muzahidulislamabir123@gmail.com>
2025-08-07 01:41:15 +0600

    updated

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,19 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBpMXV2vq9ZljbOZx3CCvaZXneXV2fEU8k",
-    authDomain: "study-buddy-study-assiatant.firebaseapp.com",
-    projectId: "study-buddy-study-assiatant",
-    storageBucket: "study-buddy-study-assiatant.appspot.com", 
-    messagingSenderId: "725712157464",
-    appId: "1:725712157464:web:ecb7f93694fcafbbd3bc6d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);  // Export the storage
+export const storage = getStorage(app);
 export default app;
